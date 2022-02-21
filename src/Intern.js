5d622579026605e1eeee966986b49bbae4386c9a
George991808 <gellis7@gmail.com>
2022-02-21 19:25:54 +0800

    created classes

---
+++ src/Intern.js
@@ -0,0 +1,17 @@
+const Employee = require("./Employee.js");
+
+class Intern extends Employee{
+    constructor(name, id, email, school, role) {
+        super(name, id, email, "Intern");
+        this.school = school
+        
+       
+    }
+    school() {
+        return this.school
+    }
+
+   
+};
+
+module.exports = Manager
