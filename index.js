74744fa2ae1c6bed52979745673f7a82044a486d
George991808 <gellis7@gmail.com>
2022-02-21 19:51:36 +0800

    all questions about manager added

--- index.js
+++ index.js
@@ -5,18 +5,38 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const questions = [
+const initialQuestions = [
     {
         type: "input",
         name: "manager",
         message: "Enter manager's name",
        
       },
-
+     {type: "input",
+      name: "id",
+      message: "Enter manager's ID number",
+    },
+      
+    {
+      type: "input",
+      name: "email",
+      message: "Enter manager's email address",
+      
+    },
+    {
+      type: "input",
+      name: "office",
+      message: "Enter manager's office number",
+    }
 ]
 const start =() => {
-    inquirer.prompt(questions).then((response)=>{
-
+    inquirer.prompt(initialQuestions).then((response)=>{
+        const manager = new Manager(
+            response.name,
+            response.id,
+            response.email,
+            response.office
+          );
     })
 }
 
